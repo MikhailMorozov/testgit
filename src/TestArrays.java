1ac804c2262af201c3935b8dae352b872bb1c93b
1 <m.s.morozoff@gmail.com>
2020-08-18 20:31:46 +0300

    in second branch

--- src/TestArrays.java
+++ src/TestArrays.java
@@ -1,10 +1,10 @@
 public class TestArrays {
     public static void main(String[] args) {
         int number = 5;
-        int[] arr = {-5, 5,8,9,15,4,5,8,3,7};
+        int[] arr = {-5, 5, 8, 9, 15, 4, 5, 8, 3, 7};
 
-        for (int i = 0; i < arr.length; i++){
-
+        for (int i = 0; i < arr.length; i++) {
+            if (number == arr[i])
         }
 
 
